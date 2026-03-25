81ab8c5810fd61955d06457c4e0745d7fd6d93ff
Rajan Singh <145154389+versatilerajan@users.noreply.github.com>
2026-03-25 19:54:38 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyATOaDyx7aCsYX9-rWurYEMCCMhevggWXs",
-  authDomain:        "jiva-5f50c.firebaseapp.com",
-  projectId:         "jiva-5f50c",
-  storageBucket:     "jiva-5f50c.firebasestorage.app",
+  apiKey: "AIzaSyATOaDyx7aCsYX9-rWurYEMCCMhevggWXs",
+  authDomain: "jiva-5f50c.firebaseapp.com",
+  projectId: "jiva-5f50c",
+  storageBucket: "jiva-5f50c.firebasestorage.app",
   messagingSenderId: "563053665604",
-  appId:             "1:563053665604:web:dfffc03c689768733ccb11",
+  appId: "1:563053665604:web:dfffc03c689768733ccb11",
 };
 
-const app             = initializeApp(firebaseConfig);
-export const auth     = getAuth(app);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const githubProvider = new GithubAuthProvider();
 export default app;
